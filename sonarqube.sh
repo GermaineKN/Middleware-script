1eaede1dce178c07135969146819b90437189df7
GermaineKN <germainekam@gmail.com>
2022-10-27 11:18:31 -0500

    New sonarqube

---
+++ sonarqube.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+## Author : Germaine
+## Date : 10/26/2022
+
+
+Description : Sonarqube installation
+
+sudo yum update -y
+sudo yum install java-11-openjdk-devel -y
+sudo yum install java-11-openjdk -y
+cd /opt
+sudo yum install wget -y
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip
+yum install unzip -y
+sudo unzip /opt/sonarqube-9.3.0.51899.zip
+sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
+cd /opt/sonarqube-9.3.0.51899/bin/linux-x86-64
+ifconfig
+sudo firewall-cmd --permanent --add-port=9000/tcp
+sudo firewall-cmd --reload
+./sonar.sh start
+./sonar.sh
